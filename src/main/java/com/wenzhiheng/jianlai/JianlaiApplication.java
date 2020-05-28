b6d6432ffa17aad631ef8c0df505c85ec35d89bf
wenzhiheng <wenzhiheng@yunmu-ai.com>
2020-05-28 17:56:16 +0800

    初始化

---
+++ src/main/java/com/wenzhiheng/jianlai/JianlaiApplication.java
@@ -0,0 +1,13 @@
+package com.wenzhiheng.jianlai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JianlaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JianlaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wenzhiheng/jianlai/JianlaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenzhiheng.jianlai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JianlaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
