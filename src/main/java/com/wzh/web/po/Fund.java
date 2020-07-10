2ddc41d50d5da1644a071bd6dfdce00f1e175189
wenzhiheng <wenzhiheng@yunmu-ai.com>
2020-07-10 16:41:54 +0800

    基金

---
+++ src/main/java/com/wzh/web/po/Fund.java
@@ -0,0 +1,182 @@
+package com.wzh.web.po;
+
+import java.sql.Date;
+
+/**
+ * @Author: wzh
+ * @Date: 2020/7/10 16:30
+ **/
+public class Fund {
+
+    private long id;
+    private long code;
+    private String jc;
+    private java.sql.Date jzrq;
+    private long dwjz;
+    private long ljjz;
+    private long rzdf;
+    private long zzf;
+    private long Yzf1;
+    private long Yzf3;
+    private long Yzf6;
+    private long Nzf1;
+    private long Nzf2;
+    private long Nzf3;
+    private long jnzf;
+    private long lnzf;
+    private long qjzf;
+    private long sxf;
+    private long md;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public String getJc() {
+        return jc;
+    }
+
+    public void setJc(String jc) {
+        this.jc = jc;
+    }
+
+    public Date getJzrq() {
+        return jzrq;
+    }
+
+    public void setJzrq(Date jzrq) {
+        this.jzrq = jzrq;
+    }
+
+    public long getDwjz() {
+        return dwjz;
+    }
+
+    public void setDwjz(long dwjz) {
+        this.dwjz = dwjz;
+    }
+
+    public long getLjjz() {
+        return ljjz;
+    }
+
+    public void setLjjz(long ljjz) {
+        this.ljjz = ljjz;
+    }
+
+    public long getRzdf() {
+        return rzdf;
+    }
+
+    public void setRzdf(long rzdf) {
+        this.rzdf = rzdf;
+    }
+
+    public long getZzf() {
+        return zzf;
+    }
+
+    public void setZzf(long zzf) {
+        this.zzf = zzf;
+    }
+
+    public long getYzf1() {
+        return Yzf1;
+    }
+
+    public void setYzf1(long yzf1) {
+        Yzf1 = yzf1;
+    }
+
+    public long getYzf3() {
+        return Yzf3;
+    }
+
+    public void setYzf3(long yzf3) {
+        Yzf3 = yzf3;
+    }
+
+    public long getYzf6() {
+        return Yzf6;
+    }
+
+    public void setYzf6(long yzf6) {
+        Yzf6 = yzf6;
+    }
+
+    public long getNzf1() {
+        return Nzf1;
+    }
+
+    public void setNzf1(long nzf1) {
+        Nzf1 = nzf1;
+    }
+
+    public long getNzf2() {
+        return Nzf2;
+    }
+
+    public void setNzf2(long nzf2) {
+        Nzf2 = nzf2;
+    }
+
+    public long getNzf3() {
+        return Nzf3;
+    }
+
+    public void setNzf3(long nzf3) {
+        Nzf3 = nzf3;
+    }
+
+    public long getJnzf() {
+        return jnzf;
+    }
+
+    public void setJnzf(long jnzf) {
+        this.jnzf = jnzf;
+    }
+
+    public long getLnzf() {
+        return lnzf;
+    }
+
+    public void setLnzf(long lnzf) {
+        this.lnzf = lnzf;
+    }
+
+    public long getQjzf() {
+        return qjzf;
+    }
+
+    public void setQjzf(long qjzf) {
+        this.qjzf = qjzf;
+    }
+
+    public long getSxf() {
+        return sxf;
+    }
+
+    public void setSxf(long sxf) {
+        this.sxf = sxf;
+    }
+
+    public long getMd() {
+        return md;
+    }
+
+    public void setMd(long md) {
+        this.md = md;
+    }
+}
