ccd41c99b46375c2a22b5f6727fd76783e58311d
wenzhiheng <zhiheng.wen@yunmu-ai.com>
2020-07-21 16:52:15 +0800

    基金

--- src/main/resources/db/fund_all.sql
+++ src/main/resources/db/fund_all.sql
@@ -1,17 +1,11 @@
-CREATE TABLE `fund_all` (
-`id`  int(11) NOT NULL AUTO_INCREMENT ,
-`code`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-`name`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
-`dwjz`  double(11,5) NOT NULL ,
-`ljjz`  double(11,5) NULL DEFAULT NULL ,
-`rzdf`  double(11,5) NULL DEFAULT NULL ,
-`rq`  date NOT NULL ,
-`create`  datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP ,
-PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
-AUTO_INCREMENT=1
-ROW_FORMAT=DYNAMIC
-;
-
+CREATE TABLE `test`.`fund_all`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '基金代码',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '基金名称',
+  `dwjz` double(11, 5) NOT NULL COMMENT '单位净值',
+  `ljjz` double(11, 5) NULL DEFAULT NULL COMMENT '累计净值',
+  `rzdf` double(11, 5) NULL DEFAULT NULL COMMENT '日增长率',
+  `rq` date NOT NULL COMMENT '日期',
+  `create` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
--- src/main/resources/db/fund_info.sql
+++ src/main/resources/db/fund_info.sql
@@ -1,14 +1,11 @@
-CREATE TABLE `fund_info` (
-`id`  int(11) NOT NULL AUTO_INCREMENT ,
-`code`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '基金代码' ,
-`name`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '基金名称' ,
-`archives`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '基金档案' ,
-PRIMARY KEY (`id`),
-UNIQUE INDEX `code1` (`code`) USING BTREE
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
-AUTO_INCREMENT=13659
-ROW_FORMAT=DYNAMIC
-;
-
+CREATE TABLE `test`.`fund_info`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '基金代码',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '基金名称',
+  `archives` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '基金档案',
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '基金类型',
+  `found` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '成立日',
+  `scope` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '规模',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `code1`(`code`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 13659 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
